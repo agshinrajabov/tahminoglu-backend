88b05b4fe7d31fc123578804ef79c9be0cb5a9f7
Agshin <agshinrajabov@Agshins-MacBook-Pro.local>
2021-02-09 15:11:29 +0400

    telegram bot update

--- telegram.js
+++ telegram.js
@@ -6,14 +6,14 @@ module.exports = function(photo) {
     channelMode: true    // Handle `channel_post` updates as messages (optional)
     }
 
-    const bot = new Telegraf('1128404304:AAEvlFmHh-yQUyzuiDdv02325R5Ed_cSBDk', options)
+    const bot = new Telegraf('1693483922:AAFPgV2jtu_Yqb1LAyTd6QKYC2yqIcz5uyY', options)
     // bot.start((ctx) => {
     // ctx.reply('Welcome!')
     // });
     bot.hears("bye", ctx => console.log(ctx.chat.id));
     // bot.help((ctx) => ctx.reply('Send me a sticker'))
     // bot.on('sticker', (ctx) => ctx.reply('👍'));
-    bot.telegram.sendPhoto('@tahminoglu', {source: fs.readFileSync(photo)}, {caption: '\n\n\nGünlük tahmin! - Türkiyenin en büyük ve ücretsiz banko iddia tahminleri uygulaması - Tahminoğlu! Profesyonel ekip tarafından yapılan günlük banko tahminler ve maç analizleri!'});
+    bot.telegram.sendPhoto('@tahminoglucom', {source: fs.readFileSync(photo)}, {caption: '\n\n\nGünlük tahmin! - Türkiyenin en büyük ve ücretsiz banko iddia tahminleri uygulaması - Tahminoğlu! Profesyonel ekip tarafından yapılan günlük banko tahminler ve maç analizleri!'});
     // bot.hears('hi', (ctx) => ctx.reply('Hey there'));
     bot.launch()
 }
